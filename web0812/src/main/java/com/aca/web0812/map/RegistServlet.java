54eb8874621ff662f0301a68963551d085cba3f0
dltmdcks98 <dltmdcks98@gmail.com>
2022-08-12 18:10:24 +0900

    SQL  연동

--- web0812/src/main/java/com/aca/web0812/map/RegistServlet.java
+++ web0812/src/main/java/com/aca/web0812/map/RegistServlet.java
@@ -8,11 +8,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.aca.web0812.domain.HotSpot;
+import com.aca.web0812.model.HotSpotDAO;
+
 //클라이언트의 등록 요청을 처리하는 서블릿
 public class RegistServlet extends HttpServlet {
 	/*클라이언트가 비동기 요청을 하면 화면전체를 바꾸려는 것이 아니라 데이터만을 교환하기 위함이므로 기존 동기방식처럼 html 페이지 전체를 보내지 말고,
 	 * 데이터만을 응답정보로 보내는게 올바르다.*/
 
+	HotSpotDAO hotSpotDAO;
+	public void init() throws ServletException {
+		hotSpotDAO= new HotSpotDAO();
+	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//파라미터 받기
 		request.setCharacterEncoding("utf-8");
@@ -25,6 +33,15 @@ public class RegistServlet extends HttpServlet {
 		System.out.println("longi"+longi);
 		System.out.println("icon"+icon);
 		System.out.println("content"+content);
+		//4개의 파라미터를 하나로 모다서 전달한 DTO 준비
+		HotSpot hotSpot = new HotSpot();
+		hotSpot.setLati(Float.parseFloat(lati));
+		hotSpot.setLongi(Float.parseFloat(longi));
+		hotSpot.setIcon(icon);
+		hotSpot.setContent(content);
+		
+		int result = hotSpotDAO.insert(hotSpot);
+		
 
 		//응답을 html로 보내지 마록, insert한 결과가 성공인지 실패인지 여부 메시지
 		//즉 복합된 결과를 보내고자할때는 xml이나 json이 많이 쓰임
@@ -34,11 +51,18 @@ public class RegistServlet extends HttpServlet {
 		//성공여부 코드, 메시지
 		
 		//out.print() 안에 인수로 넣은 스트링은 json 오브젝트는 아니고 그냥 json 표기법을 지킨 String 따라서 클라이언트는 파싱해서 사용해야함
-		String resData ="{";
+		String resData=null;
+		if(result==0) {
+		resData ="{";
 		resData+="\"code\":1,";
-		resData+="\"msg\":\"데이터 등록 성공\"";
+		resData+="\"msg\":\"데이터 등록 실패\"";
 		resData+="}";
-		
+		}else {
+			resData ="{";
+			resData+="\"code\":1,";
+			resData+="\"msg\":\"데이터 등록 성공\"";
+			resData+="}";
+		}
 		out.print(resData);
 	}
 }
