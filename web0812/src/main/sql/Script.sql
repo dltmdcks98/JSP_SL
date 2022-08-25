9a3c9dd14d89ff636b03dc6523322830405680c7
dltmdcks98 <dltmdcks98@gmail.com>
2022-08-25 17:13:58 +0900

    22.08.25 댓글 카운트 sql문

--- web0812/src/main/sql/Script.sql
+++ web0812/src/main/sql/Script.sql
@@ -27,13 +27,21 @@ START WITH 1;
 
 
 
-SELECT deptno, COUNT(*) FROM (
-	SELECT ENAME,sal,emp.DEPTNO AS deptno FROM emp,dept WHERE emp.DEPTNO = dept.DEPTNO
-) GROUP BY deptno
+--뉴스와 댓글 테이블을 합치되 일반 join (innerJOIN)으로 합치기
+SELECT * 
+FROM NEWS n, COMMENTS c 
+WHERE n.NEWS_ID =c.NEWS_ID ;
+
+--outerJOIN으로 합치기
+SELECT news_id, title ,writer ,regdate ,hit, COUNT(news_id) as cnt 
+FROM(
+	SELECT title ,writer ,regdate ,hit, c.news_id  AS news_id 
+	FROM news n LEFT OUTER JOIN  comments c 
+	ON n.news_id =c.news_id
+	) GROUP BY news_id, title ,writer, regdate ,hit
+
+--원본 news는 무조건 모든 내용이 나와야함
 
 
 
-SELECT deptno, COUNT(deptno) FROM (
-	SELECT d.deptno FROM DEPT d LEFT OUTER JOIN EMP e 
-	ON e.DEPTNO = d.DEPTNO 
-) GROUP BY deptno
+SELECT news_id, title ,writer ,regdate ,hit, COUNT(nid) as cnt FROM(SELECT n.news_id AS news_id,title ,writer ,regdate ,hit, c.news_id  AS nid FROM news n LEFT OUTER JOIN  comments c ON n.news_id =c.news_id) GROUP BY news_id, title ,writer, regdate ,hit
