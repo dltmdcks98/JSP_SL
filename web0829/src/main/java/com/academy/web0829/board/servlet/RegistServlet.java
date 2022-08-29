35ed812c847eb48b8f1f494f0169e517b1b6d217
dltmdcks98 <dltmdcks98@gmail.com>
2022-08-29 16:16:40 +0900

    22.08.29 mybatis

--- web0829/src/main/java/com/academy/web0829/board/servlet/RegistServlet.java
+++ web0829/src/main/java/com/academy/web0829/board/servlet/RegistServlet.java
@@ -1,13 +1,18 @@
 package com.academy.web0829.board.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.graalvm.compiler.word.BarrieredAccess;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.academy.web0829.domain.Board;
 
@@ -25,5 +30,24 @@ public class RegistServlet extends HttpServlet {
 		board.setWriter(writer);
 		board.setContent(content);
 		
+		//쿼리문이 들어있는 BoardMapper.xml을 이용
+		String resource = "com/academy/web0829/mybatis/config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+		//mybatis를 이용하면 쿼리문을 수행하기 위한 객체인 sqlSession객체를 이용하여 sql문을 호출할 수 있다.
+		SqlSession sqlSession  = factory.openSession();
+		int result = sqlSession.insert("babo.insert",board);
+		sqlSession.commit();
+		
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("등록 성공");
+		
+		if(result > 0 ) {
+			out.print("성공");
+		}else {
+			out.print("실패");
+		}
 	}
 }
